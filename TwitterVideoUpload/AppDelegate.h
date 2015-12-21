972b31126fbaa5858720308a0f343d1b9317707a
Trung Vo <mtrung@gmail.com>
2015-12-21 16:35:58 -0600

    Initial Commit

---
+++ TwitterVideoUpload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterVideoUpload
+//
+//  Created by Trung Vo on 12/21/15.
+//  Copyright © 2015 Trung Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterVideoUpload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterVideoUpload
+//
+//  Created by Trung Vo on 12/21/15.
+//  Copyright © 2015 Trung Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
